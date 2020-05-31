9aba8290c2599ea144dfb34b52ef06892fdcbea6
Joming He <joming.he@qq.com>
2020-06-01 00:45:29 +0800

    -

---
+++ src/gindow/Settings.php
@@ -0,0 +1,12 @@
+<?php 
+ 
+namespace Gindow; 
+ 
+class Settings
+{
+    public static function get($key)
+    {
+        return "$key: UCars\n";
+    }
+}
+
--- testing.php
+++ testing.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Hello\Gindow;
+use Gindow\Settings;
 
-echo Gindow::settings_get('appName');
+echo Settings::get('appName');
